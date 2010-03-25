1ef85d47c52b9e91c3ecf76d3db32fed944e3354
root <root@test.(none)>
2010-03-25 17:45:00 +0000

    remove dead code from demo

--- src/gd_test.erl
+++ src/gd_test.erl
@@ -7,8 +7,6 @@ test() ->
   {ok, Gd} = gd:read(Bin, "image/jpg"),
   gd:resize(Gd, 700),
   {ok, Bin2} = gd:blob(Gd, 100),
-  Port = open_port({spawn, "open -f -a preview"}, [binary, stream, use_stdio, out]),
-  port_command(Port, Bin2),
-  unlink(Port),
-  port_close(Port).
+  file:write_file("priv/riak_resized.jpg", Bin2).
+
   
